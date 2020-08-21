09204cc5732a2aa80b0bcb4f9a8db20f3734333a
punit <>
2020-08-21 17:17:34 +0530

    initial commit

---
+++ src/main/java/com/datarecm/service/DataRecMApplication.java
@@ -0,0 +1,13 @@
+package com.datarecm.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRecMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRecMApplication.class, args);
+	}
+
+}
