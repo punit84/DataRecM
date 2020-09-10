ec87fe00ff4a3f0a5c606a44797e108deb89e90d
Punit <punit.15884@gmail.com>
2020-09-10 12:32:32 +0530

    some refactoring to isoloate glue

--- src/main/java/com/datarecm/service/DataRecMApplication.java
+++ src/main/java/com/datarecm/service/DataRecMApplication.java
@@ -1,27 +1,13 @@
 package com.datarecm.service;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.glue.AWSGlue;
-import com.amazonaws.services.glue.AWSGlueClientBuilder;
-import com.amazonaws.services.glue.model.Column;
 import com.amazonaws.services.glue.model.Database;
-import com.amazonaws.services.glue.model.Partition;
-import com.amazonaws.services.glue.model.StorageDescriptor;
-import com.amazonaws.services.glue.model.Table;
-import com.datarecm.service.glue.datacatalog.util.TableWithPartitions;
-import com.google.gson.Gson;
 
 /**
  * <p>
@@ -44,84 +30,12 @@ public class DataRecMApplication {
 		//String topicArn = Optional.ofNullable(System.getenv("sns_topic_arn_gdc_replication_planner")).orElse("arn:aws:sns:ap-south-1:436386478328:GlueExportSNSTopic");
 		String ddbTblNameForDBStatusTracking = Optional.ofNullable(System.getenv("ddb_name_gdc_replication_planner"))
 				.orElse("ddb_name_gdc_replication_planner");
-
 		// Print environment variables
 		printEnvVariables(sourceGlueCatalogId, null, ddbTblNameForDBStatusTracking, dbPrefixString, separator);
-		// Create Objects for Glue
-		AWSGlue glue = AWSGlueClientBuilder.standard().withRegion(region).build();
-
-		Database glueDB = glueService.selectDB(sourceGlueCatalogId,glue);
-
-		Table table= glueService.selectTable(sourceGlueCatalogId, glueDB, glue);
-		List<Partition> partitionList = glueService.getGlueUtil().getPartitions(glue, sourceGlueCatalogId, table.getDatabaseName(), table.getName());
-		System.out.printf("\nDatabase: '%s', Table: %s, num_partitions: %d \n", table.getDatabaseName(), table.getName(), partitionList.size());
-
-		Map<String, String> map= table.getParameters();
-		StorageDescriptor tableDesc = table.getStorageDescriptor();
-		List<Column> columns= tableDesc.getColumns();
-		System.out.printf("\ncolumns: %d \n", columns.size());
-		//System.out.printf("\ncolumns: %d \n", map.get));
-
-		for (Column column : columns) {
-		//	System.out.println(column.getName() +":"+column.getType());
-			System.out.println(column.toString());
-
-		}
-		//System.out.println(columns.toString());
-		
-		
-		//Gson gson = new Gson();
-		// Convert Table to JSON String
-		//String tableDDL = gson.toJson(table);
-		//System.out.println(new AttributeValue().withS(tableDDL));
-
-//		TableWithPartitions tableWithParts = new TableWithPartitions();
-//		tableWithParts.setPartitionList(partitionList);
-//		tableWithParts.setTable(table);
-//		Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
-//		item.put("table_id", new AttributeValue().withS(table.getName().concat("|").concat(table.getDatabaseName())));
-//		item.put("source_glue_catalog_id", new AttributeValue().withS(sourceGlueCatalogId));
-//		item.put("table_schema", new AttributeValue().withS(tableDDL)); 
-//		item.put("is_large_table", new AttributeValue().withS(Boolean.toString(false)));
-
+		glueService.glueOperation(region, sourceGlueCatalogId);
 
 	}
 
-	/**
-	 * Tokenize the Data Prefix String to a List of Prefixes
-	 * @param dbPrefixString
-	 * @param token
-	 * @return
-	 */
-	public static List<String> tokenizeDatabasePrefixString(String str, String separator) {
-
-		List<String> dbPrefixesList = Collections.list(new StringTokenizer(str, separator)).stream()
-				.map(token -> (String) token)
-				.collect(Collectors.toList());
-		System.out.println("Number of database prefixes: " + dbPrefixesList.size());
-		return dbPrefixesList;
-	}
-
-	/**
-	 * 
-	 * @param dBList
-	 * @param requiredDBPrefixList
-	 * @return
-	 */
-	public static List<Database> getRequiredDatabases(List<Database> dBList, List<String> dbPrefixesList){
-
-		List<Database> dBsToExportList = new ArrayList<Database>();
-		for(Database database : dBList) {
-			for(String dbPrefix : dbPrefixesList) {
-				if(database.getName().toLowerCase().startsWith(dbPrefix)) {
-					dBsToExportList.add(database);
-					break;
-				}
-			}
-		}
-		System.out.printf("Number of databases in Glue Catalog: %d, number of databases to be exported: %d \n", dBList.size(), dBsToExportList.size());
-		return dBsToExportList;
-	}
 	/**
 	 * This method prints environment variables
 	 * @param sourceGlueCatalogId
--- src/main/java/com/datarecm/service/GlueService.java
+++ src/main/java/com/datarecm/service/GlueService.java
@@ -1,15 +1,21 @@
 package com.datarecm.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.glue.AWSGlue;
+import com.amazonaws.services.glue.AWSGlueClientBuilder;
+import com.amazonaws.services.glue.model.Column;
 import com.amazonaws.services.glue.model.Database;
 import com.amazonaws.services.glue.model.Partition;
+import com.amazonaws.services.glue.model.StorageDescriptor;
 import com.amazonaws.services.glue.model.Table;
 import com.datarecm.service.glue.datacatalog.util.GlueUtil;
 import com.datarecm.service.glue.datacatalog.util.TableWithPartitions;
@@ -51,11 +57,11 @@ public class GlueService {
 		return destDB;
 
 	}
-	
+
 	public Table selectTable(String sourceGlueCatalogId,Database db,AWSGlue glue) {
 
 		List<Table> dbTableList = glueUtil.getTables(glue, sourceGlueCatalogId, db.getName());
-		
+
 		System.out.println("Choose Tables from following:");
 
 		for (int i = 0; i < dbTableList.size(); i++) {
@@ -73,7 +79,7 @@ public class GlueService {
 		return table;
 
 	}
-	
+
 	public List<Map<String, AttributeValue>> getAllDB(String sourceGlueCatalogId,AWSGlue glue) {
 
 		// Create Objects for Utility classes
@@ -137,4 +143,78 @@ public class GlueService {
 		return keyentered;
 	}
 
+	public void glueOperation(String region, String sourceGlueCatalogId) {
+		// Create Objects for Glue
+		AWSGlue glue = AWSGlueClientBuilder.standard().withRegion(region).build();
+
+		Database glueDB = selectDB(sourceGlueCatalogId,glue);
+
+		Table table= selectTable(sourceGlueCatalogId, glueDB, glue);
+		List<Partition> partitionList = getGlueUtil().getPartitions(glue, sourceGlueCatalogId, table.getDatabaseName(), table.getName());
+		System.out.printf("\nDatabase: '%s', Table: %s, num_partitions: %d \n", table.getDatabaseName(), table.getName(), partitionList.size());
+
+		Map<String, String> map= table.getParameters();
+		StorageDescriptor tableDesc = table.getStorageDescriptor();
+		List<Column> columns= tableDesc.getColumns();
+		System.out.printf("\ncolumns: %d \n", columns.size());
+		//System.out.printf("\ncolumns: %d \n", map.get));
+
+		for (Column column : columns) {
+			//	System.out.println(column.getName() +":"+column.getType());
+			System.out.println(column.toString());
+
+		}
+		//System.out.println(columns.toString());
+
+
+		//Gson gson = new Gson();
+		// Convert Table to JSON String
+		//String tableDDL = gson.toJson(table);
+		//System.out.println(new AttributeValue().withS(tableDDL));
+
+		//		TableWithPartitions tableWithParts = new TableWithPartitions();
+		//		tableWithParts.setPartitionList(partitionList);
+		//		tableWithParts.setTable(table);
+		//		Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
+		//		item.put("table_id", new AttributeValue().withS(table.getName().concat("|").concat(table.getDatabaseName())));
+		//		item.put("source_glue_catalog_id", new AttributeValue().withS(sourceGlueCatalogId));
+		//		item.put("table_schema", new AttributeValue().withS(tableDDL)); 
+		//		item.put("is_large_table", new AttributeValue().withS(Boolean.toString(false)));
+	}
+
+	/**
+	 * Tokenize the Data Prefix String to a List of Prefixes
+	 * @param dbPrefixString
+	 * @param token
+	 * @return
+	 */
+	public List<String> tokenizeDatabasePrefixString(String str, String separator) {
+
+		List<String> dbPrefixesList = Collections.list(new StringTokenizer(str, separator)).stream()
+				.map(token -> (String) token)
+				.collect(Collectors.toList());
+		System.out.println("Number of database prefixes: " + dbPrefixesList.size());
+		return dbPrefixesList;
+	}	
+	
+	/**
+	 * 
+	 * @param dBList
+	 * @param requiredDBPrefixList
+	 * @return
+	 */
+	public List<Database> getRequiredDatabases(List<Database> dBList, List<String> dbPrefixesList){
+
+		List<Database> dBsToExportList = new ArrayList<Database>();
+		for(Database database : dBList) {
+			for(String dbPrefix : dbPrefixesList) {
+				if(database.getName().toLowerCase().startsWith(dbPrefix)) {
+					dBsToExportList.add(database);
+					break;
+				}
+			}
+		}
+		System.out.printf("Number of databases in Glue Catalog: %d, number of databases to be exported: %d \n", dBList.size(), dBsToExportList.size());
+		return dBsToExportList;
+	}
 }
